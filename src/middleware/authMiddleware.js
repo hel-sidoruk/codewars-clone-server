a097b3be65571f13a0751620a372a401cb43b20d
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-01-26 16:58:43 +0300

    docs: update readme.md

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,12 +4,12 @@ module.exports = function (req, res, next) {
   try {
     const token = req.headers.authorization.split(' ')[1];
     if (!token) {
-      return res.status(401).json({ message: 'User not authorized' });
+      return res.status(401).json({ message: 'User is not authorized' });
     }
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
     req.user = decoded;
     next();
   } catch (e) {
-    res.status(401).json({ message: 'User not authorized' });
+    res.status(401).json({ message: 'User is not authorized' });
   }
 };
