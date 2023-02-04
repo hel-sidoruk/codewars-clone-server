484730ed405d947c48f73093324f341137c29b0b
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-02-04 14:36:02 +0300

    feat: add methods for adding trained/solved katas

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -1,3 +1,4 @@
+const ApiError = require('../error/ApiError');
 const { Accounts } = require('../models/models');
 
 class AccountController {
@@ -5,6 +6,62 @@ class AccountController {
     const { username } = req.user;
     const data = await Accounts.findOne({ where: { username } });
     return res.json(data);
+  }
+
+  async addSolvedKata(req, res, next) {
+    try {
+      const { username } = req.user;
+      const accountData = await Accounts.findOne({ where: { username } });
+      const { kataId } = req.body;
+
+      let { solvedKatas, trainedKatas } = accountData;
+      if (!kataId) return next(ApiError.badRequest('No kata id provided'));
+      if (solvedKatas.includes(kataId)) return res.json('Kata already solved');
+      trainedKatas = trainedKatas.filter((kata) => kata !== kataId);
+
+      solvedKatas.push(kataId);
+
+      await Accounts.update(
+        { solvedKatas, trainedKatas },
+        {
+          where: {
+            username,
+          },
+        }
+      );
+
+      return res.json({ status: 'ok' });
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
+  }
+
+  async addTrainedKata(req, res, next) {
+    try {
+      const { username } = req.user;
+      const accountData = await Accounts.findOne({ where: { username } });
+      const { kataId } = req.body;
+
+      const { trainedKatas } = accountData;
+      if (!kataId) return next(ApiError.badRequest('No kata id provided'));
+      if (trainedKatas.includes(kataId))
+        return res.json('Kata already trained');
+
+      trainedKatas.push(kataId);
+
+      await Accounts.update(
+        { trainedKatas },
+        {
+          where: {
+            username,
+          },
+        }
+      );
+
+      return res.json({ status: 'ok' });
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
   }
 }
 
--- src/routes/accountRouter.js
+++ src/routes/accountRouter.js
@@ -4,5 +4,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 const router = new Router();
 
 router.get('/', authMiddleware, AccountController.getInfo);
+router.patch('/solved', authMiddleware, AccountController.addSolvedKata);
+router.patch('/trained', authMiddleware, AccountController.addTrainedKata);
 
 module.exports = router;
