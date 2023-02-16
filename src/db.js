4da7e76e7827b77b3a2d963c1745349f283f0de9
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-02-16 20:43:01 +0300

    fix: fix database connection

--- src/db.js
+++ src/db.js
@@ -1,12 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-  }
-);
+module.exports = new Sequelize(process.env.DB_LINK, {
+  dialect: 'postgres',
+  port: process.env.DB_PORT,
+});
